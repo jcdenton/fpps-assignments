f263e9578fe0d8f78fd05554af5d5d4027478059
jcdenton <ivan.bruzgin@gmail.com>
2013-10-03 18:19:04 +0300

    Added Week 1: Functions & Evaluations assignment solution

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,32 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 && r == 0) 1 else if (c < 0 || c > r) 0 else pascal(c - 1, r - 1) + pascal(c, r - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balance(chars: List[Char], count: Int, waitingForClosingParenthesis: Boolean): Boolean =
+      if (chars.isEmpty) (!waitingForClosingParenthesis && (count == 0))
+      else if (chars.head == '(') balance(chars.tail, count + 1, true)
+      else if (chars.head == ')') balance(chars.tail, count - 1, false)
+      else balance(chars.tail, count, waitingForClosingParenthesis)
+
+    balance(chars, 0, false)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    val ways = Array.fill(money + 1)(0)
+    ways(0) = 1
+    coins.foreach(coin =>
+      for (j <- coin to money)
+        ways(j) = ways(j) + ways(j - coin)
+    )
+    ways(money)
+  }
 }
